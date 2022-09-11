177e925add303b86c789126c7b05e413d28e0dcb
artiom.labietskii <artiom.labietskii@gmail.com>
2022-09-11 16:37:25 +0300

    Add cosii lab1.

--- 7 term/SPOLKS/Spolks1/Server/Program.cs
+++ 7 term/SPOLKS/Spolks1/Server/Program.cs
@@ -4,6 +4,6 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-Server server = Server.GetInstance("192.168.0.11", "60000");
+Server server = Server.GetInstance("192.168.0.14", "60000");
 server.StartListening();
 
