acdbb2ca22eb2a89356cbce241364405aac30248
artiom.labietskii <artiom.labietskii@gmail.com>
2022-09-23 20:02:27 +0300

    Mpsis lab3

---
+++ 7 term/MPSIS/Lab3/Lab3/Debug/main.d
@@ -0,0 +1,21 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: F:/CCS/ccs/ccs_base/msp430/include/msp430.h
+main.obj: F:/CCS/ccs/ccs_base/msp430/include/msp430f5529.h
+main.obj: F:/CCS/ccs/ccs_base/msp430/include/in430.h
+main.obj: F:/CCS/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics.h
+main.obj: F:/CCS/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics_legacy_undefs.h
+
+../main.c:
+
+F:/CCS/ccs/ccs_base/msp430/include/msp430.h:
+
+F:/CCS/ccs/ccs_base/msp430/include/msp430f5529.h:
+
+F:/CCS/ccs/ccs_base/msp430/include/in430.h:
+
+F:/CCS/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics.h:
+
+F:/CCS/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics_legacy_undefs.h:
+
