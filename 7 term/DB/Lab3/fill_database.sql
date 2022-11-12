5ccae73aab62713a93ed3d11060daf61bd1fb8bc
dude <artiom.labietskii@gmail.com>
2022-11-12 11:10:21 +0300

    Updated db3 script.

--- 7 term/DB/Lab3/fill_database.sql
+++ 7 term/DB/Lab3/fill_database.sql
@@ -1,10 +1,130 @@
-INSERT INTO product (id, name, price, number) VALUES ('0', 'coil', '5332', '21850');
-INSERT INTO product (id, name, price, number) VALUES ('1', 'coil', '8577', '162638');
-INSERT INTO product (id, name, price, number) VALUES ('2', 'coil', '6854', '429018');
-INSERT INTO product (id, name, price, number) VALUES ('3', 'rice', '5400', '587657');
-INSERT INTO product (id, name, price, number) VALUES ('4', 'beer', '712', '913815');
-INSERT INTO product (id, name, price, number) VALUES ('5', 'coil', '7343', '309942');
-INSERT INTO product (id, name, price, number) VALUES ('6', 'paper', '6576', '252565');
-INSERT INTO product (id, name, price, number) VALUES ('7', 'rice', '3241', '501930');
-INSERT INTO product (id, name, price, number) VALUES ('8', 'coil', '7226', '31320');
-INSERT INTO product (id, name, price, number) VALUES ('9', 'beer', '4269', '156309');
+INSERT INTO product (id, name, price, number) VALUES ('0', 'paper', '1502', '126716');
+INSERT INTO product (id, name, price, number) VALUES ('1', 'coil', '1106', '401569');
+INSERT INTO product (id, name, price, number) VALUES ('2', 'beer', '1483', '460946');
+INSERT INTO product (id, name, price, number) VALUES ('3', 'paper', '80', '13164');
+INSERT INTO product (id, name, price, number) VALUES ('4', 'coil', '733', '149558');
+INSERT INTO product (id, name, price, number) VALUES ('5', 'paper', '6264', '973528');
+INSERT INTO product (id, name, price, number) VALUES ('6', 'rice', '2659', '230388');
+INSERT INTO product (id, name, price, number) VALUES ('7', 'rice', '2949', '769238');
+INSERT INTO product (id, name, price, number) VALUES ('8', 'beer', '6094', '708018');
+INSERT INTO product (id, name, price, number) VALUES ('9', 'beer', '9432', '547602');
+INSERT INTO supplier (id, name, address, phone_number) VALUES ('0', 'Fowler LLC', '52170 Christopher Drive Suite 321
+Philipbury, VT 95995', '261-807-0796x103');
+INSERT INTO supplier (id, name, address, phone_number) VALUES ('1', 'Rojas Inc', '11199 Miles Key Apt. 377
+North Michael, AZ 84159', '001-136-492-7341x8565');
+INSERT INTO supplier (id, name, address, phone_number) VALUES ('2', 'Doyle, Smith and Moon', '737 Parks Crossing
+Lake Virginiachester, WI 43762', '+1-187-273-6282');
+INSERT INTO supplier (id, name, address, phone_number) VALUES ('3', 'Hayes-Harding', '122 Kevin Fields
+Lake John, FM 68909', '566-871-9502x2625');
+INSERT INTO supplier (id, name, address, phone_number) VALUES ('4', 'Morris-Hale', '90444 Burke Dale
+Melanieton, CA 43837', '001-767-705-1642x453');
+INSERT INTO supplier (id, name, address, phone_number) VALUES ('5', 'Martin PLC', '7313 Candice Spring
+Fuentesside, LA 70200', '110-006-9697x841');
+INSERT INTO supplier (id, name, address, phone_number) VALUES ('6', 'Porter-Sanchez', '095 Sparks Glens
+West Danielport, AS 26335', '224.642.5172x83514');
+INSERT INTO supplier (id, name, address, phone_number) VALUES ('7', 'Johnson LLC', '32793 Sandra Circles Apt. 639
+Port Jacobville, WA 91116', '001-144-437-7146x206');
+INSERT INTO supplier (id, name, address, phone_number) VALUES ('8', 'Calhoun, White and King', '6500 Virginia Trail
+Dianeside, AK 43514', '+1-441-612-6555x01701');
+INSERT INTO supplier (id, name, address, phone_number) VALUES ('9', 'Campbell-Mitchell', '884 Rebecca Overpass
+South Chad, ID 04657', '+1-177-093-2161x83111');
+INSERT INTO shipper (id, name, address, postal_code) VALUES ('0', 'Hampton Ltd', '349 Mitchell Row Suite 419
+Port Justinchester, DC 04558', '(696)645-4345x201');
+INSERT INTO shipper (id, name, address, postal_code) VALUES ('1', 'Wright, Carter and Ferrell', '0374 Jennifer Ridge Suite 962
+North Terristad, OR 74967', '273.571.5196x535');
+INSERT INTO shipper (id, name, address, postal_code) VALUES ('2', 'Wolf-Bruce', '84631 Barber Landing Apt. 620
+Jonfurt, IN 43756', '(005)732-7775x03167');
+INSERT INTO shipper (id, name, address, postal_code) VALUES ('3', 'Green, Ruiz and Hayes', '738 Jacob Estates Apt. 957
+South Lori, TX 04575', '456.961.5087x22697');
+INSERT INTO shipper (id, name, address, postal_code) VALUES ('4', 'Hayes, Owens and Walsh', '316 Chelsea Ridges
+Millsside, MD 65291', '(101)460-2348x1425');
+INSERT INTO shipper (id, name, address, postal_code) VALUES ('5', 'Palmer and Sons', 'USNS Briggs
+FPO AE 38499', '734.993.9638');
+INSERT INTO shipper (id, name, address, postal_code) VALUES ('6', 'Peters Inc', '2675 Brown Lights Suite 850
+Walshberg, SC 17357', '314-076-8328x0470');
+INSERT INTO shipper (id, name, address, postal_code) VALUES ('7', 'Ruiz-Hunter', '86816 Jordan Flat Suite 348
+New Andrew, MS 57012', '220.412.1964x072');
+INSERT INTO shipper (id, name, address, postal_code) VALUES ('8', 'Foster-Hawkins', '42511 Quinn Roads
+Hendrixfurt, NC 74849', '001-593-514-7197x089');
+INSERT INTO shipper (id, name, address, postal_code) VALUES ('9', 'Andersen, Wilson and Parsons', 'USS Alvarez
+FPO AA 62472', '+1-719-594-0140');
+INSERT INTO warehouse (id, address, phone_number, supplier_id) VALUES ('0', '790 James Via Suite 259
+North Sarah, MN 21810', '(401)826-0552x240', '14920');
+INSERT INTO warehouse (id, address, phone_number, supplier_id) VALUES ('1', '126 Smith Mission
+Susanmouth, MS 99253', '(154)512-5409', '61215');
+INSERT INTO warehouse (id, address, phone_number, supplier_id) VALUES ('2', '3760 Clark Field
+Victorburgh, MT 54986', '001-146-424-2862', '3332');
+INSERT INTO warehouse (id, address, phone_number, supplier_id) VALUES ('3', '03389 Turner Underpass
+Emilymouth, CA 23718', '001-508-694-9724x4999', '13940');
+INSERT INTO warehouse (id, address, phone_number, supplier_id) VALUES ('4', '98878 Crystal Vista
+Matthewview, MA 82013', '8815846214', '79540');
+INSERT INTO warehouse (id, address, phone_number, supplier_id) VALUES ('5', '53513 Shawn Falls Suite 535
+Duanemouth, AL 75933', '3430993234', '56467');
+INSERT INTO warehouse (id, address, phone_number, supplier_id) VALUES ('6', '4001 Ramirez Plain Apt. 415
+East Alexandra, VA 89388', '001-039-414-3234x85393', '3395');
+INSERT INTO warehouse (id, address, phone_number, supplier_id) VALUES ('7', '054 Jeffrey Springs Suite 496
+North Jamesshire, OH 53909', '583-041-1619x11831', '6614');
+INSERT INTO warehouse (id, address, phone_number, supplier_id) VALUES ('8', '537 Ellis Burg Suite 837
+South Katie, AS 95714', '4125053349', '96840');
+INSERT INTO warehouse (id, address, phone_number, supplier_id) VALUES ('9', '1139 Nash Radial
+South Jeromestad, VI 42903', '001-317-662-7399x81707', '17758');
+INSERT INTO freight (id, start_date, end_date, destination, customer_id) VALUES ('0', '1993-02-05', '2009-11-14', '25671 Williams Valleys
+Meganport, OK 20267', '9291');
+INSERT INTO freight (id, start_date, end_date, destination, customer_id) VALUES ('1', '1971-08-18', '2004-02-10', 'USNS Rodriguez
+FPO AE 25845', '3072');
+INSERT INTO freight (id, start_date, end_date, destination, customer_id) VALUES ('2', '2006-11-30', '1977-06-29', '818 Anne Common
+North Jacksonton, MI 65195', '19094');
+INSERT INTO freight (id, start_date, end_date, destination, customer_id) VALUES ('3', '2005-07-21', '1995-03-25', 'USNV Thompson
+FPO AE 04259', '86645');
+INSERT INTO freight (id, start_date, end_date, destination, customer_id) VALUES ('4', '1974-10-16', '2008-05-28', '690 Townsend Coves Apt. 505
+Sarahtown, DC 42681', '68381');
+INSERT INTO freight (id, start_date, end_date, destination, customer_id) VALUES ('5', '1998-09-14', '1975-03-26', '24303 Solomon Stream Apt. 696
+New Destiny, MH 75042', '82089');
+INSERT INTO freight (id, start_date, end_date, destination, customer_id) VALUES ('6', '2011-01-17', '2000-11-11', '1981 Henson Centers Suite 691
+Hartshire, ME 85109', '22146');
+INSERT INTO freight (id, start_date, end_date, destination, customer_id) VALUES ('7', '1990-08-25', '1980-04-10', '6809 Young Mall
+East Jasmineshire, MN 70180', '87990');
+INSERT INTO freight (id, start_date, end_date, destination, customer_id) VALUES ('8', '1985-10-21', '1995-09-04', '29343 Hoffman Oval
+East Heather, OK 41196', '568');
+INSERT INTO freight (id, start_date, end_date, destination, customer_id) VALUES ('9', '2006-12-27', '1985-06-07', '332 Keith Mission
+North Deborahbury, FM 24570', '73042');
+INSERT INTO driver (id, name, passport_number, freight_id, shipper_id) VALUES ('0', 'Deborah Yoder', '76317', '2224', '19923');
+INSERT INTO driver (id, name, passport_number, freight_id, shipper_id) VALUES ('1', 'Jacqueline Wilson', '10956', '11348', '46197');
+INSERT INTO driver (id, name, passport_number, freight_id, shipper_id) VALUES ('2', 'Greg Sanders', '21060', '27306', '18150');
+INSERT INTO driver (id, name, passport_number, freight_id, shipper_id) VALUES ('3', 'Amber Knight', '34830', '76713', '66709');
+INSERT INTO driver (id, name, passport_number, freight_id, shipper_id) VALUES ('4', 'Kimberly Armstrong', '19341', '16699', '82741');
+INSERT INTO driver (id, name, passport_number, freight_id, shipper_id) VALUES ('5', 'John Lewis', '26367', '33805', '25889');
+INSERT INTO driver (id, name, passport_number, freight_id, shipper_id) VALUES ('6', 'Kevin Taylor', '20525', '42516', '41535');
+INSERT INTO driver (id, name, passport_number, freight_id, shipper_id) VALUES ('7', 'Jason Moody', '87394', '35663', '58190');
+INSERT INTO driver (id, name, passport_number, freight_id, shipper_id) VALUES ('8', 'Frances Delgado', '52741', '97316', '17661');
+INSERT INTO driver (id, name, passport_number, freight_id, shipper_id) VALUES ('9', 'Ronald Thomas', '552', '14509', '19137');
+INSERT INTO truck (id, registration_number, max_load, driver_id) VALUES ('0', '72469', '970', '88735');
+INSERT INTO truck (id, registration_number, max_load, driver_id) VALUES ('1', '75102', '403', '31904');
+INSERT INTO truck (id, registration_number, max_load, driver_id) VALUES ('2', '21025', '779', '69083');
+INSERT INTO truck (id, registration_number, max_load, driver_id) VALUES ('3', '30897', '394', '78380');
+INSERT INTO truck (id, registration_number, max_load, driver_id) VALUES ('4', '77809', '411', '59537');
+INSERT INTO truck (id, registration_number, max_load, driver_id) VALUES ('5', '1644', '719', '43022');
+INSERT INTO truck (id, registration_number, max_load, driver_id) VALUES ('6', '67064', '508', '51640');
+INSERT INTO truck (id, registration_number, max_load, driver_id) VALUES ('7', '69151', '876', '4372');
+INSERT INTO truck (id, registration_number, max_load, driver_id) VALUES ('8', '35963', '915', '18672');
+INSERT INTO truck (id, registration_number, max_load, driver_id) VALUES ('9', '12057', '803', '70108');
+INSERT INTO customer (id, name, address, phone_number) VALUES ('0', 'Tanya Wagner', '440 Darrell Gateway Apt. 898
+Richardsburgh, MO 34155', '001-224-893-7683');
+INSERT INTO customer (id, name, address, phone_number) VALUES ('1', 'Monique Morris', '0832 Davis Station Apt. 927
+North Timothy, CT 50455', '+1-426-923-8450');
+INSERT INTO customer (id, name, address, phone_number) VALUES ('2', 'Brianna Barron', '611 Jacob Square
+South Thomas, PA 32390', '5561566521');
+INSERT INTO customer (id, name, address, phone_number) VALUES ('3', 'John Cannon', '7243 Chandler Ramp
+New Joshuaborough, IA 38745', '639-463-2042x73518');
+INSERT INTO customer (id, name, address, phone_number) VALUES ('4', 'Angela Miller', '566 Jennifer Turnpike Apt. 929
+East Erin, OR 86817', '8943406644');
+INSERT INTO customer (id, name, address, phone_number) VALUES ('5', 'Joanne Clayton', '3610 Cherry Corner Suite 144
+East Carlafort, OK 16713', '001-221-911-1656x6689');
+INSERT INTO customer (id, name, address, phone_number) VALUES ('6', 'Cheryl Lopez', '7713 Tyler Extension
+Hannahchester, ID 08058', '626-809-8472x741');
+INSERT INTO customer (id, name, address, phone_number) VALUES ('7', 'Shane Mann', 'Unit 9255 Box 4778
+DPO AP 85440', '001-740-699-0287x408');
+INSERT INTO customer (id, name, address, phone_number) VALUES ('8', 'Gary Haynes', '762 Mark Islands Apt. 747
+Pamelamouth, KY 53817', '830-225-5698');
+INSERT INTO customer (id, name, address, phone_number) VALUES ('9', 'Catherine Fox', '99443 Austin Islands
+West Jennifermouth, ME 21571', '+1-541-006-8625x7093');
--- 7 term/DB/Lab3/generate_filler_script.py
+++ 7 term/DB/Lab3/generate_filler_script.py
@@ -40,6 +40,56 @@ def generate_products(number):
 		products.append((i, faker.product(), faker.random_number(digits=4), faker.random_number(digits=6)))
 	return products
 
+def generate_suppliers(number):
+	suppliers = []
+	
+	for i in range(number):
+		suppliers.append((i, faker.company(), faker.address(), faker.phone_number()))
+	return suppliers
+
+def generate_shippers(number):
+	shippers = []
+	
+	for i in range(number):
+		shippers.append((i, faker.company(), faker.address(), faker.phone_number()))
+	return shippers
+
+def generate_warehouses(number):
+	warehouses = []
+	
+	for i in range(number):
+		warehouses.append((i, faker.address(), faker.phone_number(), faker.random_number(digits = 5))) #id of real supplier must be here
+	return warehouses
+
+def generate_freights(number):
+	freights = []
+	
+	for i in range(number):
+		freights.append((i, faker.date(), faker.date(), faker.address(), faker.random_number(digits = 5))) # id of real customer must be here
+	return freights
+
+def generate_drivers(number):
+	drivers = []
+	
+	for i in range(number):
+		drivers.append((i, faker.name(), str(faker.random_number(digits = 5)), faker.random_number(digits = 5), faker.random_number(digits = 5))) # id of real freight and shipper  must be here
+	return drivers
+
+def generate_trucks(number):
+	trucks = []
+	
+	for i in range(number):
+		trucks.append((i, str(faker.random_number(digits = 5)), faker.random_number(digits = 3), faker.random_number(digits = 5))) # change registration number and max load generation, insert real driver id
+	return trucks
+
+def generate_customers(number):
+	customers = []
+	
+	for i in range(number):
+		customers.append((i, faker.name(), faker.address(), faker.phone_number()))
+	return customers
+
+
 
 f =  open("fill_database.sql", "w")
 product_columns = ["id", "name", "price", "number"]
@@ -52,3 +102,10 @@ truck_columns = ["id", "registration_number", "max_load", "driver_id"]
 customer_columns = ["id", "name", "address", "phone_number"]
 
 insert(f, "product", product_columns, generate_products(10))
+insert(f, "supplier", supplier_columns, generate_suppliers(10))
+insert(f, "shipper", shipper_columns, generate_shippers(10))
+insert(f, "warehouse", warehouse_columns, generate_warehouses(10))
+insert(f, "freight", freight_columns, generate_freights(10))
+insert(f, "driver", driver_columns, generate_drivers(10))
+insert(f, "truck", truck_columns, generate_trucks(10))
+insert(f, "customer", customer_columns, generate_customers(10))
