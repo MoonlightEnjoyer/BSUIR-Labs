44a5213e41c828ba39b33518edc58902d17f048f
The-Postal-Dude <artiom.labietskii@gmail.com>
2022-11-16 12:23:12 +0300

    M

---
+++ 7 term/MOD/Lab5/Lab5/Lab5/Program.cs
@@ -0,0 +1,110 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+int x1 = 37;
+int a = 131;
+int c = 1021;
+int m = 100;
+double d = 0.001;
+
+double lambda = 0.1;
+int mu_param = 2;
+
+int currentState = 0;
+int prevNumber = x1;
+List<double> randomProbabilities = new List<double>();
+List<double> fList = new List<double>();
+List<double> gList = new List<double>();
+
+for (int i = 0; i < 100; i++)
+{
+    var generatedNumber = RandomNumber(prevNumber);
+    prevNumber = generatedNumber;
+    randomProbabilities.Add((double)generatedNumber / 100);
+    fList.Add(F(randomProbabilities[i]));
+    gList.Add(G(randomProbabilities[i]));
+}
+
+Console.WriteLine("PROBABILITIES:");
+foreach (var p in randomProbabilities)
+{
+    Console.Write($"{p} ");
+}
+Console.WriteLine();
+
+Console.WriteLine("F functions:");
+foreach (var f in fList)
+{
+    Console.Write($"{f} ");
+}
+Console.WriteLine();
+
+
+Console.WriteLine("G functions:");
+foreach (var g in gList)
+{
+    Console.Write($"{g} ");
+}
+Console.WriteLine();
+
+Console.WriteLine("Average service time:");
+Console.WriteLine(AverageServiceTime(gList));
+
+Console.WriteLine("Average staying time:");
+Console.WriteLine(AverageStaying(AverageDowntime(Downtime(gList, fList)), AverageServiceTime(gList)));
+
+Console.WriteLine("Load percentage:");
+Console.WriteLine(LoadPercentage(fList, gList, Downtime(gList, fList)));
+
+int RandomNumber(int prevNum)
+{
+    return (a * prevNum + c) % m;
+}
+
+double F(double p)
+{
+    double log = Math.Log(1 - p) * (-1);
+    return Math.Round(log / lambda);
+}
+
+double G(double p)
+{
+    double log = Math.Log(1 - p) * (-1);
+    double temp = Math.Sqrt(log) * mu_param;
+    return Math.Round(temp, 1);
+}
+
+double AverageServiceTime(List<double> gList)
+{
+    return gList.Average();
+}
+
+List<double> Downtime(List<double> gList, List<double> fList)
+{
+    List<double> downtime = new List<double>();
+    double delay;
+    for (int i = 0; i < fList.Count - 1; i++)
+    {
+        delay = gList[i] = fList[i + 1];
+        if (delay > 0)
+        {
+            downtime.Add(delay);
+        }
+    }
+
+    return downtime;
+}
+
+double AverageDowntime(List<double> downtime)
+{
+    return downtime.Average();
+}
+
+double AverageStaying(double averageDowntime, double averageService)
+{
+    return averageDowntime + averageService;
+}
+
+double LoadPercentage(List<double> fList, List<double> gList, List<double> downtime)
+{
+    return gList.Sum() / fList.Sum() * 100 + downtime.Sum();
+}
