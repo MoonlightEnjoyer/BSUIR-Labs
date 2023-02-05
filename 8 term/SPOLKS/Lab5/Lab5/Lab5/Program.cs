ea2e5aee6f1310b81b282ffd7eead63f31ae536c
The-Postal-Dude <artiom.labietskii@gmail.com>
2023-02-05 21:38:57 +0300

    spolks

--- 8 term/SPOLKS/Lab5/Lab5/Lab5/Program.cs
+++ 8 term/SPOLKS/Lab5/Lab5/Lab5/Program.cs
@@ -3,22 +3,20 @@ using System.Net;
 using System.Text;
 using System.Collections.Concurrent;
 
-//Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.Icmp);
 Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.Icmp);
 ConcurrentDictionary<string, List<string>> routes = new ConcurrentDictionary<string, List<string>>();
 Mutex mutex = new Mutex();
-Thread thread1 = new Thread(() => Traceroute("yandex.ru", s, routes));
+Thread thread1 = new Thread(() => Traceroute("youtube.com", s, routes));
 Thread thread2 = new Thread(() => Traceroute("google.com", s, routes));
 Thread thread3 = new Thread(() => Traceroute("stackoverflow.com", s, routes));
 
-//var result = Traceroute("google.com", s); 
 thread1.Start();
 thread2.Start();
 thread3.Start();
 thread1.Join();
 thread2.Join();
 thread3.Join();
-int i = 1;
+
 foreach (var route in routes)
 {
     Console.WriteLine($"Route to {route.Key}:");
@@ -30,14 +28,13 @@ foreach (var route in routes)
 
 void Traceroute(string hostName, Socket socket, ConcurrentDictionary<string, List<string>> routes)
 {
-    mutex.WaitOne();
-    Console.WriteLine($"Thread id: {(short)Thread.CurrentThread.ManagedThreadId}");
+    //mutex.WaitOne();
     List<string> nodes = new List<string>();
     byte[] data = new byte[1024];
     int recv = 0;
     IPHostEntry iphe = Dns.GetHostEntry(hostName);
     IPEndPoint iep = new IPEndPoint(iphe.AddressList[0], 0);
-    EndPoint ep = (EndPoint)iep;
+    EndPoint ep = iep;
     ICMP packet = new ICMP();
 
     packet.Type = 0x08;
@@ -48,38 +45,40 @@ void Traceroute(string hostName, Socket socket, ConcurrentDictionary<string, Lis
     packet.SequenceNumber = 1;
     //Buffer.BlockCopy(BitConverter.GetBytes(Thread.CurrentThread.ManagedThreadId), 0, packet.Message, 0, 2);
     //Buffer.BlockCopy(BitConverter.GetBytes(1), 0, packet.Message, 2, 2);
-    data = Encoding.ASCII.GetBytes(hostName);
+    data = Encoding.ASCII.GetBytes("qwertyuiop");
     Buffer.BlockCopy(data, 0, packet.Message, 0, data.Length);
     packet.MessageSize = data.Length;
-    int packetsize = packet.MessageSize;
+    int packetsize = packet.MessageSize + 8;
 
     UInt16 chcksum = packet.getChecksum();
     packet.Checksum = chcksum;
-
     socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, 3000);
 
     int badcount = 0;
 
     for (int i = 1; i < 256; i++)
     {
+        mutex.WaitOne();
         socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.IpTimeToLive, i);
 
         DateTime timestart = DateTime.Now;
 
         socket.SendTo(packet.getBytes(), packetsize, SocketFlags.None, iep);
+        mutex.ReleaseMutex();
+
         try
         {
             ICMP response;
             data = new byte[1024];
             byte[] ddd = new byte[1024];
+            
             while (true)
             {
                 byte[] tid = BitConverter.GetBytes((short)Thread.CurrentThread.ManagedThreadId);
                 byte[] id;
-                
-                recv = socket.ReceiveFrom(data, SocketFlags.Peek, ref ep);
+                mutex.WaitOne();
+                recv = socket.Receive(data, SocketFlags.Peek);
 
-                recv = socket.ReceiveFrom(data, ref ep);
                 response = new ICMP(data, recv);
                 if (response.Type == 11)
                 {
@@ -87,7 +86,7 @@ void Traceroute(string hostName, Socket socket, ConcurrentDictionary<string, Lis
                 }
                 else
                 {
-                    id = data[52..54];
+                    id = data[24..26];
                 }
                 
                 //recv = socket.ReceiveFrom(ddd, SocketFlags.Peek, ref ep);
@@ -97,11 +96,15 @@ void Traceroute(string hostName, Socket socket, ConcurrentDictionary<string, Lis
                     //recv = socket.ReceiveFrom(ddd, SocketFlags.Peek, ref ep);
 
                     recv = socket.ReceiveFrom(ddd, ref ep);
+                    mutex.ReleaseMutex();
                     break;
                 }
+                mutex.ReleaseMutex();
             }
 
             
+
+
             TimeSpan timestop = DateTime.Now - timestart;
 
             if (response.Type == 11)
@@ -140,5 +143,5 @@ void Traceroute(string hostName, Socket socket, ConcurrentDictionary<string, Lis
         routes.TryAdd(hostName, nodes);
     }
 
-    mutex.ReleaseMutex();
+    //mutex.ReleaseMutex();
 }
