4cfd4e639f4e12b3c58401d1efecc7c4d4777125
artiom.labietskii <artiom.labietskii@gmail.com>
2023-02-06 00:59:16 +0300

    spolks

--- 8 term/SPOLKS/Lab5/Lab5/Lab5/Program.cs
+++ 8 term/SPOLKS/Lab5/Lab5/Lab5/Program.cs
@@ -3,19 +3,38 @@ using System.Net;
 using System.Text;
 using System.Collections.Concurrent;
 
+List<Thread> threads = new List<Thread>();
+
 Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.Icmp);
 ConcurrentDictionary<string, List<string>> routes = new ConcurrentDictionary<string, List<string>>();
 Mutex mutex = new Mutex();
-Thread thread1 = new Thread(() => Traceroute("youtube.com", s, routes));
-Thread thread2 = new Thread(() => Traceroute("google.com", s, routes));
-Thread thread3 = new Thread(() => Traceroute("stackoverflow.com", s, routes));
 
-thread1.Start();
-thread2.Start();
-thread3.Start();
-thread1.Join();
-thread2.Join();
-thread3.Join();
+
+foreach (var arg in args[1..])
+{
+    if (args[0] == "ping")
+    {
+        threads.Add(new Thread(() => Ping(arg, s)));
+    }
+    else if (args[0] == "traceroute")
+    {
+        threads.Add(new Thread(() => Traceroute(arg, s, routes)));
+    }
+    else if (args[0] == "smurf")
+    {
+
+    }
+}
+
+foreach (var thread in threads)
+{
+    thread.Start();
+}
+
+foreach (var thread in threads)
+{
+    thread.Join();
+}
 
 foreach (var route in routes)
 {
@@ -28,7 +47,6 @@ foreach (var route in routes)
 
 void Traceroute(string hostName, Socket socket, ConcurrentDictionary<string, List<string>> routes)
 {
-    //mutex.WaitOne();
     List<string> nodes = new List<string>();
     byte[] data = new byte[1024];
     int recv = 0;
@@ -142,6 +160,84 @@ void Traceroute(string hostName, Socket socket, ConcurrentDictionary<string, Lis
     {
         routes.TryAdd(hostName, nodes);
     }
+}
 
-    //mutex.ReleaseMutex();
+void Ping(string hostName, Socket socket)
+{
+    byte[] data = new byte[1024];
+    int recv = 0;
+    IPHostEntry iphe = Dns.GetHostEntry(hostName);
+    IPEndPoint iep = new IPEndPoint(iphe.AddressList[0], 0);
+    EndPoint ep = iep;
+    ICMP packet = new ICMP();
+
+    packet.Type = 0x08;
+
+    packet.Code = 0x00;
+    packet.Checksum = 0;
+    packet.Id = (short)Thread.CurrentThread.ManagedThreadId;
+    packet.SequenceNumber = 1;
+    data = Encoding.ASCII.GetBytes("qwertyuiop");
+    Buffer.BlockCopy(data, 0, packet.Message, 0, data.Length);
+    packet.MessageSize = data.Length;
+    int packetsize = packet.MessageSize + 8;
+
+    UInt16 chcksum = packet.getChecksum();
+    packet.Checksum = chcksum;
+    socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, 3000);
+    
+    mutex.WaitOne();
+
+    DateTime timestart = DateTime.Now;
+
+    socket.SendTo(packet.getBytes(), packetsize, SocketFlags.None, iep);
+    mutex.ReleaseMutex();
+
+    try
+    {
+        ICMP response;
+        data = new byte[1024];
+
+        while (true)
+        {
+            byte[] tid = BitConverter.GetBytes((short)Thread.CurrentThread.ManagedThreadId);
+            byte[] id;
+            mutex.WaitOne();
+            recv = socket.Receive(data, SocketFlags.Peek);
+
+            response = new ICMP(data, recv);
+            if (response.Type == 0)
+            {
+                id = data[24..26];
+            }
+            else
+            {
+                id = data[24..26];
+            }
+
+            if (tid[0] == id[0] && tid[1] == id[1])
+            {
+                recv = socket.ReceiveFrom(data, ref ep);
+                mutex.ReleaseMutex();
+                break;
+            }
+            mutex.ReleaseMutex();
+        }
+
+        TimeSpan timestop = DateTime.Now - timestart;
+
+        if (response.Type == 0)
+        {
+            Console.WriteLine($"Ответ от {hostName} получен.");
+        }
+        else
+        {
+            //problem text
+        }
+     }
+    catch (SocketException)
+    {
+        Console.WriteLine($"не удалось получить ответ от {hostName}");
+    }
+    
 }
